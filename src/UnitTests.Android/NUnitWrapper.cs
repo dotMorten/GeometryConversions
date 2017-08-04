1dcdd588c10f060caa4ffa42b37a65ab0b7089f0
Morten Nielsen <no@spam.thx>
2017-08-04 16:00:05 -0700

    Adds Android support. Note: Due to a problem with the nuget package, this won't build without modifying the package

---
+++ src/UnitTests.Android/NUnitWrapper.cs
@@ -0,0 +1,18 @@
+﻿namespace Microsoft.VisualStudio.TestTools.UnitTesting
+{
+    public class TestClassAttribute : NUnit.Framework.TestFixtureAttribute { }
+
+    public class TestMethodAttribute : NUnit.Framework.TestAttribute { }
+
+    public class Assert : NUnit.Framework.Assert
+    {
+        public static void IsInstanceOfType(object obj, System.Type type)
+        {
+            NUnit.Framework.Assert.IsInstanceOfType(type, obj, null);
+        }
+        public static void IsInstanceOfType(object obj, System.Type type, string message)
+        {
+            NUnit.Framework.Assert.IsInstanceOfType(type, obj, message);
+        }
+    }
+}
