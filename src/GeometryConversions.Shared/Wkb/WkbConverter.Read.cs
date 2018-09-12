86635412a840e3d26e89f44a4409620b34a9da46
Matvei Stefarov <mstefarov@esri.com>
2018-09-12 09:45:52 -0700

    WKB: Fix a couple problems with reading polygon and multipolygon
    Fixed off-by-1 error in number of rings, and avoid non-resettable IEnumerables when passing to Runtime constructors.

--- src/GeometryConversions.Shared/Wkb/WkbConverter.Read.cs
+++ src/GeometryConversions.Shared/Wkb/WkbConverter.Read.cs
@@ -150,14 +150,21 @@ namespace GeometryConversions.Wkb
 		{
 			// Get the Number of rings in this Polygon.
 			int numRings = (int)readUInt32(reader, byteOrder);
-			return new Polygon(CoordinateCollectionEnumerator(numRings + 1, reader, byteOrder, type, null), spatialReference);
+		    List<IEnumerable<MapPoint>> rings = new List<IEnumerable<MapPoint>>();
+		    foreach (var ring in CoordinateCollectionEnumerator(numRings, reader, byteOrder, type, null))
+		    {
+		        rings.Add(new List<MapPoint>(ring));
+		    }
+			return new Polygon(rings, spatialReference);
 			
 		}
+
 		IEnumerable<IEnumerable<MapPoint>> CoordinateCollectionEnumerator(int count, BinaryReader reader, WkbByteOrder byteOrder, WkbGeometryType type, SpatialReference spatialReference)
 		{
 			for(int i=0;i<count;i++)
 				yield return ReadCoordinates(reader, byteOrder, type, spatialReference);
 		}
+
 		private Multipoint ReadWkbMultiPoint(BinaryReader reader, WkbByteOrder byteOrder, WkbGeometryType type, SpatialReference spatialReference)
 		{
 			// Get the number of points in this multipoint.
@@ -225,7 +232,10 @@ namespace GeometryConversions.Wkb
 				// TODO: Validate type
 
 				int numRings = (int)readUInt32(reader, byteOrder);
-				rings.AddRange(CoordinateCollectionEnumerator(numRings + 1, reader, byteOrder, type, spatialReference));
+			    foreach (var ring in CoordinateCollectionEnumerator(numRings, reader, byteOrder, type, spatialReference))
+			    {
+			        rings.Add(new List<MapPoint>(ring));
+			    }
 			}
 
 			//Create and return the MultiPolygon.
